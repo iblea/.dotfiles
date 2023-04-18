de5a0581f5aea48ea1dafcc7be77fda1c425c2d1
jhh9232 <iblea@iasdf.com>
2023-04-19 08:09:00 +0900

    root 권한을 가진 사용자 user 생성 스크립트

---
+++ useradd.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERNAME=test
+PASSWORD="testtest"
+SHELL_PATH=/bin/bash
+
+
+is_exist=`cat /etc/passwd | grep "^$USERNAME:"`
+if [[ $is_exist != "" ]]; then
+	echo "user name exist"
+	exit
+fi
+
+HOME_PATH=/home/$USERNAME
+if [ ! -d $HOME_PATH ]; then
+	mkdir $HOME_PATH
+else
+	echo "home directory exist"
+fi
+# useradd
+useradd $USERNAME -d $HOME_PATH
+
+# /etc/passwd change
+sed -i "s#^$USERNAME:x:.*#$USERNAME:x:0:0::$HOME_PATH:$SHELL_PATH#" /etc/passwd
+
+# /etc/group change
+sed -i "s#^$USERNAME:x:.*#$USERNAME:x:0:#" /etc/group
+
+# password setting
+yes $PASSWORD | passwd $USERNAME
+
