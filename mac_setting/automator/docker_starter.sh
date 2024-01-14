5b0cba364bfaabf8b00ccc395f5856cdacca13e0
iblea <iblea@iasdf.com>
2024-01-15 08:58:39 +0900

    docker 자동 시작 automater 스크립트 추가

---
+++ mac_setting/automator/docker_starter.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+already_docker=$(ps -aef | grep "docker serve .*\.sock" | grep -v "grep")
+if [ -n "$already_docker" ]; then
+	echo "docker service is already running."
+	exit 0
+fi
+
+open /Applications/Docker.app
+
+cnt=0
+while [ : ] ; do
+	if [ $cnt -ge 10 ]; then
+		echo "docker start failed"
+		exit 1
+		# break;
+	fi
+
+	cnt=$(expr $cnt + 1)
+	sleep 1
+	docker_find=$(ps -aef | grep "docker serve .*\.sock" | grep -v "grep")
+
+	if [ -z "$docker_find" ]; then
+		echo "cannot find docker process, continue and wait 1 second"
+		continue;
+	fi
+
+	echo "docker start"
+	docker start volado
+	break;
+done
+
+echo "done"
+
