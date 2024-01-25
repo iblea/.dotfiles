ec3efe1d39a5062bd9d872a346166f80acb2d2df
iblea <iblea@iasdf.com>
2024-01-26 08:13:08 +0900

    폰트에 따른 initial cols, rows 변경

--- wezterm/ui.lua
+++ wezterm/ui.lua
@@ -32,16 +32,18 @@ return {
     -- freetype_load_target = 'Normal',
     -- freetype_render_target = 'Normal',
 
+    dpi = 144.0,
     -- font_size = 10.0,
+    -- initial_cols = 140,
+    -- initial_rows = 35,
     font_size = 19.0,
+    initial_cols = 80,
+    initial_rows = 18,
     line_height = 1.3,
     cell_width = 0.85,
     underline_thickness = 0,
     underline_position = "-3pt",
-    dpi = 144.0,
     -- adjust_window_size_when_changing_font_size = false,
-    initial_rows = 35,
-    initial_cols = 140,
 
     font = wezterm_font_config,
     font_rules = {
