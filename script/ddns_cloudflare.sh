9988a2363a4a4e4ce7fef4623ceb921549e3c425
iblea <iblea@iasdf.com>
2024-06-24 15:15:03 +0900

    cloudflare 도메인 레코드 자동 업데이트 스크립트 추가

---
+++ script/ddns_cloudflare.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# https://developers.cloudflare.com/api/operations/dns-records-for-a-zone-create-dns-record
+# https://developers.cloudflare.com/fundamentals/api/get-started/create-token/
+
+### CloudFlare A Recoard Updater
+### DOMAIN : 변경할 도메인 입력
+### RECORD_TYPE : 변경 또는 추가할 레코드의 타입 입력
+### RECORD : 변경 또는 추가할 레코드 입력
+### CHANGED_IP : 변경/추가할 레코드의 IP 입력
+### TTL : 120과 2147483647 사이의 값 입력, 1 입력시 자동
+### Proxied : true 또는 false 입력
+### To force updating, run with -f
+
+CLOUD_FLARE_API_KEY="CLOUDFLARE_API_KEY (https://developers.cloudflare.com/fundamentals/api/get-started/create-token/)"
+DOMAIN="domain.com"
+RECORD_TYPE="A"
+# RECORD_TYPE="TXT"
+RECORD="test.domain.com"
+CHANGED_IP="192.168.0.1"
+TTL=600
+Proxied=false
+
+
+
+
+
+
+
+
+# H1="-HX-Auth-Email:$Login_Email"
+# H2="-HX-Auth-Key:$Global_API_Key"
+# H3="-HContent-Type:application/json"
+H1="Authorization: Bearer $CLOUD_FLARE_API_KEY"
+H2="Content-Type: application/json"
+
+# verify the API key
+# curl -sk --request GET \
+#     --url "https://api.cloudflare.com/client/v4/user/tokens/verify" \
+#     -H "$H1" -H "$H2"
+# exit 0
+
+V4="https://api.cloudflare.com/client/v4/zones"
+
+function getZone() {
+    ZN=$(curl -s --request GET --url "$V4?name=$DOMAIN" \
+        -H "$H1" -H "$H2" | grep -Po '(?<="id":")[^"]*' | head -1)
+    echo "$ZN"
+}
+function AID() {
+    curl -sk --request GET --url "$V4/$ZN/dns_records?name=$RECORD" \
+        -H "$H1" -H "$H2" | grep -Po '(?<="id":")[^"]*' | head -1
+}
+
+
+
+
+# get the zone id
+ZN=$(getZone)
+# ZN="ZONEID 직접입력"
+
+AIDARY=$(AID)
+# echo "AIDARY: $AIDARY"
+
+if [ -n "$AIDARY" ]; then
+    # update
+
+    echo "domain is already exist, update it"
+    curl -sk --request PUT \
+        --url "$V4/$ZN/dns_records/${AIDARY}" \
+        -H "$H1" -H "$H2" \
+        --data "{\"type\":\"${RECORD_TYPE}\",\"name\":\"${RECORD}\",\"content\":\"${CHANGED_IP}\",\"proxied\":$Proxied,\"ttl\":$TTL}" \
+        | grep -Po '(?<="name":")[^"]*|(?<="content":")[^"]*|(?<=Z"},)[^}]*|(?<="success":false,)[^$]*|(?<=\s\s)[^$]*' | xargs
+
+    exit 0
+fi
+
+
+echo "domain is not exist, create it"
+curl -sk --request POST \
+    --url "$V4/$ZN/dns_records" \
+    -H "$H1" -H "$H2" \
+    --data "{\"type\":\"${RECORD_TYPE}\",\"name\":\"${RECORD}\",\"content\":\"${CHANGED_IP}\",\"proxied\":$Proxied,\"ttl\":$TTL}" \
+    | grep -Po '(?<="name":")[^"]*|(?<="content":")[^"]*|(?<=Z"},)[^}]*|(?<="success":false,)[^$]*|(?<=\s\s)[^$]*' | xargs
+
+
+
+
+
+
+
+
+
