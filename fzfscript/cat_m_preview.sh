6a20238560f0f2bcb86babf251b2c41e95804ba6
iblea <iblea@iasdf.com>
2024-07-01 14:54:01 +0900

    fzfscript 추가

---
+++ fzfscript/cat_m_preview.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+if ! grep -q ":" <<< "$1"; then
+    echo "Wrong input"
+    exit 1
+fi
+
+
+filename=$(echo "$1" | awk -F ':' '{print $1}')
+line=$(echo "$1" | awk -F ':' '{print $2}')
+
+file_end_line=$(wc -l "$filename" | awk '{print $1}')
+
+
+startline=$(expr $line - 10)
+endline=$(expr $line + 10)
+
+if [ $startline -le 0 ]; then
+    startline=1
+fi
+
+if [ $endline -ge $file_end_line ]; then
+    endline=$file_end_line
+fi
+
+# sed -n "${startline},${endline}p" ${filename}
+
+if [[ "$line" = "$startline" ]]; then
+    endline=$(expr $line + 20)
+    endline_start=$(expr $line + 1)
+
+    sed -n "${line}p" ${filename} | grep --color=always -F "$2"
+    sed -n "${endline_start},${endline}p" ${filename}
+
+    exit 0
+fi
+
+
+if [[ "$line" = "$endline" ]]; then
+    startline=$(expr $line - 20)
+    startline_end=$(expr $line - 1)
+
+    sed -n "${startline},${startline_end}p" ${filename}
+    sed -n "${line}p" ${filename} | grep --color=always -F "$2"
+
+    exit 0
+fi
+
+
+startline_end=$(expr $line - 1)
+endline_start=$(expr $line + 1)
+
+sed -n "${startline},${startline_end}p" ${filename}
+sed -n "${line}p" ${filename} | grep --color=always -F "$2"
+sed -n "${endline_start},${endline}p" ${filename}
+
+
---
+++ fzfscript/grepfzf.sh
@@ -0,0 +1,102 @@
+#!/bin/zsh
+
+
+function grepfzf_function() {
+
+    if [ -z "$(command -v fzf)" ]; then
+        agrepm "$@"
+        return
+    fi
+    if [ -z "$(command -v code)" ]; then
+        agrepn "$@"
+        return
+    fi
+
+    # code -s > /dev/null
+    # if [ $? -ne 0 ]; then
+    #     return
+    # fi
+    local exclude_dirs=( ".git" ".svn" "node_modules" "__pycache__" )
+    local search_str=()
+    for exclude_dir in "${exclude_dirs[@]}"; do
+        search_str+=( "--exclude-dir='${exclude_dir}'" )
+    done
+    if [[ "$1" = "-f" ]]; then
+        IFS='' search_str+=( "${@:2}" )
+    elif [[ "$1" = "-n" ]]; then
+        IFS='' search_str+=( "${@:2}" )
+    else
+        IFS='' search_str+=( "$@" )
+    fi
+    local current_pwd="$(pwd)"
+
+    # -P option : perl regex
+    # grep --color=always -rnIHP ${search_str[@]} | sed -e "p" -e "s/:/ | /2" | sed -n "2~2p"
+    # grep --color=always -rnIHP ${search_str[@]} | sed -e "s/:/ | /2" -e "s|\[m\[K\s*|[m[K|g"
+    # output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s/:/ | /2")
+    local output=""
+    if [[ "$1" = "-f" ]]; then
+        # echo "-f option"
+        # output=$(grep --color=always -rnIHF ${search_str[@]} | sed -e "s/:/ | /2")
+        output=$(grep --color=always -rnIHF ${search_str[@]} | sed -e "s|\:\[m\[K\s*|[m[K\n|2")
+    elif [[ "$1" = "-n" ]]; then
+        # echo "-n option"
+        # output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s/:/ | /2")
+        output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s|\:\[m\[K\s*|[m[K\n|2")
+    else
+        # echo "other option"
+        # output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s/:/ | /2")
+        output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s|\:\[m\[K\s*|[m[K\n|2")
+    fi
+
+
+    if [ -z "$output" ]; then
+        echo "grep result is empty"
+        return
+    fi
+
+
+    local ORG_IFS=$IFS
+    local output_array=()
+
+    # current shell
+    local CURRENT_SHELL=$(ps -p $$ -o 'comm=')
+
+    IFS=$'\n' output_array=( $(echo "$output" ) )
+
+    IFS=$ORG_IFS
+    local results=""
+
+    if grep -q "/zsh" <<< "$CURRENT_SHELL"; then
+        for (( i=1; i<=${#output_array[@]}; i+=2 )); do
+            if [ -z "${output_array[$i]}" ]; then
+                continue
+            fi
+            # results+="\n${output_array[$i+1]}    | ${output_array[$i]}"
+            results+="${output_array[$i]}\n${output_array[$i+1]}\n\0"
+            # results+="${output_array[$i]}\n${output_array[$i+1]}\0"
+        done
+    else
+        for (( i=0; i<${#output_array[@]}; i+=2 )); do
+            if [ -z "${output_array[$i]}" ]; then
+                continue
+            fi
+            # results+="\n${output_array[$i+1]}    | ${output_array[$i]}"
+            results+="${output_array[$i]}\n${output_array[$i+1]}\n\0"
+            # results+="${output_array[$i]}\n${output_array[$i+1]}\0"
+        done
+    fi
+
+
+    if [ -z "$results" ]; then
+        echo "cannot find '$@'"
+        return
+    fi
+
+    results=${results:0:${#results}-4}
+    echo "${results}"
+
+}
+
+
+grepfzf_function "$@"
