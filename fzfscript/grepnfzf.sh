ee88962c5f71c29f80d21a78122ab54c52ea8a90
iblea <iblea@iasdf.com>
2024-07-01 15:56:01 +0900

    grepnfzf 명령어 오류 수정

--- fzfscript/grepnfzf.sh
+++ fzfscript/grepnfzf.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 
 function grepfzf_function() {
@@ -17,16 +17,13 @@ function grepfzf_function() {
     for exclude_dir in "${exclude_dirs[@]}"; do
         search_str+=( "--exclude-dir='${exclude_dir}'" )
     done
-    IFS='' search_str+=( "$@" )
-
-    # if [[ "$1" = "-f" ]]; then
-    #     IFS='' search_str+=( "${@:2}" )
-    # elif [[ "$1" = "-n" ]]; then
-    #     IFS='' search_str+=( "${@:2}" )
-    # else
-    #     IFS='' search_str+=( "$@" )
-    # fi
-
+    if [[ "$1" = "-f" ]]; then
+        IFS='' search_str+=( "${@:2}" )
+    elif [[ "$1" = "-n" ]]; then
+        IFS='' search_str+=( "${@:2}" )
+    else
+        IFS='' search_str+=( "$@" )
+    fi
     local current_pwd="$(pwd)"
 
     # -P option : perl regex
@@ -37,19 +34,6 @@ function grepfzf_function() {
     local output=""
     output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s|\:\[m\[K\s*|[m[K\n|2")
 
-    # if [[ "$1" = "-f" ]]; then
-    #     # echo "-f option"
-    #     # output=$(grep --color=always -rnIHF ${search_str[@]} | sed -e "s/:/ | /2")
-    #     output=$(grep --color=always -rnIHF ${search_str[@]} | sed -e "s|\:\[m\[K\s*|[m[K\n|2")
-    # elif [[ "$1" = "-n" ]]; then
-    #     # echo "-n option"
-    #     # output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s/:/ | /2")
-    #     output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s|\:\[m\[K\s*|[m[K\n|2")
-    # else
-    #     # echo "other option"
-    #     # output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s/:/ | /2")
-    #     output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s|\:\[m\[K\s*|[m[K\n|2")
-    # fi
 
     if [ -z "$output" ]; then
         echo "grep result is empty"
@@ -68,34 +52,21 @@ function grepfzf_function() {
     IFS=$ORG_IFS
     local results=""
 
-    if grep -q "zsh" <<< "$CURRENT_SHELL"; then
-        for (( i=1; i<=${#output_array[@]}; i+=2 )); do
-            if [ -z "${output_array[$i]}" ]; then
-                continue
-            fi
-            # results+="\n${output_array[$i+1]}    | ${output_array[$i]}"
-            results+="${output_array[$i]}\n${output_array[$i+1]}\n\0"
-            # results+="${output_array[$i]}\n${output_array[$i+1]}\0"
-        done
-    else
-        for (( i=0; i<${#output_array[@]}; i+=2 )); do
-            if [ -z "${output_array[$i]}" ]; then
-                continue
-            fi
-            # results+="\n${output_array[$i+1]}    | ${output_array[$i]}"
-            results+="${output_array[$i]}\n${output_array[$i+1]}\n\0"
-            # results+="${output_array[$i]}\n${output_array[$i+1]}\0"
-        done
-    fi
-
+    for (( i=0; i<${#output_array[@]}; i+=2 )); do
+        if [ -z "${output_array[$i]}" ]; then
+            continue
+        fi
+        # results+="\n${output_array[$i+1]}    | ${output_array[$i]}"
+        results+="${output_array[$i]}\n${output_array[$i+1]}\n\0"
+        # results+="${output_array[$i]}\n${output_array[$i+1]}\0"
+    done
 
     if [ -z "$results" ]; then
         echo "cannot find '$@'"
         return
     fi
 
-    results=${results:0:${#results}-4}
-    echo "${results}"
+    echo -e "${results}"
 
 }
 
