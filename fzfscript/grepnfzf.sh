ed27e14f2c1e5f7b57e77059b80e285d94c59c6c
iblea <iblea@iasdf.com>
2024-07-12 20:50:21 +0900

    fix: 함수 wrapping 제거

--- fzfscript/grepnfzf.sh
+++ fzfscript/grepnfzf.sh
@@ -1,56 +1,48 @@
 #!/bin/bash
 
 
-function grepfzf_function() {
+if [ -z "$@" ]; then
+    echo "no input"
+    exit 1
+fi
 
-    if [ -z "$@" ]; then
-        echo "no input"
-        return
+# code -s > /dev/null
+# if [ $? -ne 0 ]; then
+#     return
+# fi
+
+current_pwd="$(pwd)"
+
+source $HOME/.dotfiles/grepscript/grep_exclude
+search_str=( )
+IFS='' search_str+=( ${exclude_str[@]} )
+
+IFS='' search_str+=( "$@" )
+
+output=""
+output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s|\:\[m\[K\s*|[m[K\n|2")
+
+if [ -z "$output" ]; then
+    echo "grep result is empty"
+    exit 0
+fi
+
+output_array=()
+IFS=$'\n' output_array=( $(echo "$output" ) )
+
+results=""
+for (( i=0; i<${#output_array[@]}; i+=2 )); do
+    if [ -z "${output_array[$i]}" ]; then
+        continue
     fi
+    # results+="\n${output_array[$i+1]}    | ${output_array[$i]}"
+    results+="${output_array[$i]}\n${output_array[$i+1]}\n\0"
+    # results+="${output_array[$i]}\n${output_array[$i+1]}\0"
+done
 
-    # code -s > /dev/null
-    # if [ $? -ne 0 ]; then
-    #     return
-    # fi
+if [ -z "$results" ]; then
+    echo "cannot find '$@'"
+    exit 1
+fi
 
-    local current_pwd="$(pwd)"
-
-    source $HOME/.dotfiles/grepscript/grep_exclude
-    local search_str=( )
-    IFS='' search_str+=( ${exclude_str[@]} )
-
-    IFS='' search_str+=( "$@" )
-
-    local output=""
-    output=$(grep --color=always -rnIHP ${search_str[@]} | sed -e "s|\:\[m\[K\s*|[m[K\n|2")
-
-    if [ -z "$output" ]; then
-        echo "grep result is empty"
-        return
-    fi
-
-    local output_array=()
-    IFS=$'\n' output_array=( $(echo "$output" ) )
-
-    local results=""
-
-    for (( i=0; i<${#output_array[@]}; i+=2 )); do
-        if [ -z "${output_array[$i]}" ]; then
-            continue
-        fi
-        # results+="\n${output_array[$i+1]}    | ${output_array[$i]}"
-        results+="${output_array[$i]}\n${output_array[$i+1]}\n\0"
-        # results+="${output_array[$i]}\n${output_array[$i+1]}\0"
-    done
-
-    if [ -z "$results" ]; then
-        echo "cannot find '$@'"
-        return
-    fi
-
-    echo -n -e "${results}"
-
-}
-
-
-grepfzf_function "$@"
+echo -n -e "${results}"
