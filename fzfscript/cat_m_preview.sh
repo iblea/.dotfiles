53a67c1af378510f2d18b4a030096eb063b5211b
iblea <iblea@iasdf.com>
2024-09-11 14:16:45 +0900

    grepff bat 추가

--- fzfscript/cat_m_preview.sh
+++ fzfscript/cat_m_preview.sh
@@ -31,24 +31,43 @@ if [[ "$line" = "$startline" ]]; then
     endline=$(expr $line + $preview_len + $preview_len)
     endline_start=$(expr $line + 1)
 
-    sed -n "${line}p" ${filename} | grep --color=always -F "$2"
-    sed -n "${endline_start},${endline}p" ${filename}
+    if [ -z "$(command -v bat)" ]; then
+        sed -n "${line}p" ${filename} | grep --color=always -F "$2"
+        sed -n "${endline_start},${endline}p" ${filename}
+    else
+        # bat -n --theme="Dracula" --color=always --line-range "${line}:${endline}"--highlight-line "$line" ${filename}
+        bat -n --theme="Dracula" --color=always --line-range "${line}" --highlight-line "$line" ${filename} | grep --color=always -F "$2"
+        bat -n --theme="Dracula" --color=always --line-range "${endline_start}:${endline}" ${filename}
+    fi
 
 elif [[ "$line" = "$endline" ]]; then
 
     startline=$(expr $line - $preview_len - $preview_len)
     startline_end=$(expr $line - 1)
 
-    sed -n "${startline},${startline_end}p" ${filename}
-    sed -n "${line}p" ${filename} | grep --color=always -F "$2"
+    if [ -z "$(command -v bat)" ]; then
+        sed -n "${startline},${startline_end}p" ${filename}
+        sed -n "${line}p" ${filename} | grep --color=always -F "$2"
+    else
+        # bat -n --theme="Dracula" --color=always --line-range "${startline}:${line}" --highlight-line "$line" ${filename}
+        bat -n --theme="Dracula" --color=always --line-range "${startline}:${startline_end}" ${filename}
+        bat -n --theme="Dracula" --color=always --line-range "${line}" --highlight-line "$line" ${filename} | grep --color=always -F "$2"
+    fi
 
 else
 
     startline_end=$(expr $line - 1)
     endline_start=$(expr $line + 1)
 
-    sed -n "${startline},${startline_end}p" ${filename}
-    sed -n "${line}p" ${filename} | grep --color=always -F "$2"
-    sed -n "${endline_start},${endline}p" ${filename}
+    if [ -z "$(command -v bat)" ]; then
+        sed -n "${startline},${startline_end}p" ${filename}
+        sed -n "${line}p" ${filename} | grep --color=always -F "$2"
+        sed -n "${endline_start},${endline}p" ${filename}
+    else
+        # bat -n --theme="Dracula" --color=always --line-range "${startline}:${endline}" --highlight-line "$line" ${filename}
+        bat -n --theme="Dracula" --color=always --line-range "${startline}:${startline_end}" ${filename}
+        bat -n --theme="Dracula" --color=always --line-range "${line}" --highlight-line "$line" ${filename} | grep --color=always -F "$2"
+        bat -n --theme="Dracula" --color=always --line-range "${endline_start}:${endline}" ${filename}
+    fi
 
 fi
