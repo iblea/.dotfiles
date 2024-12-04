f62a96cc2369d3bf4fd9918b09cdfa58195ceeeb
iblea <iblea@iasdf.com>
2024-12-04 10:35:20 +0900

    bash bind 문법 오류 수정

--- bindkey.sh
+++ bindkey.sh
@@ -27,15 +27,15 @@ elif grep -q "/bash" <<< "$SHELL"; then
     bind '"\C-k": previous-history'
     bind '"\C-j": next-history'
 
-	bindkey "\C-x" "backward-delete-char"
-	bindkey "\C-a" "backward-char"
-	bindkey "\C-d" "forward-char"
-	bindkey "\C-w" "forward-word"
-	bindkey "\C-t" "backward-word"
-	bindkey "\C-u" "backward-word"
-	bindkey "\C-f" "forward-char"
-	bindkey "\C-b" "backward-char"
-	bindkey "\C-n" "accept-line"
-	bindkey "\C-o" "beginning-of-line"
+	bind '"\C-x": backward-delete-char'
+	bind '"\C-a": backward-char'
+	bind '"\C-d": forward-char'
+	bind '"\C-w": forward-word'
+	bind '"\C-t": backward-word'
+	bind '"\C-u": backward-word'
+	bind '"\C-f": forward-char'
+	bind '"\C-b": backward-char'
+	bind '"\C-n": accept-line'
+	bind '"\C-o": beginning-of-line'
 
 fi
