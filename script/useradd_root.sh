0c90b17c7e20fe1cf868d5aed3cefb1d110a8bb3
iblea <iblea@iasdf.com>
2025-05-30 08:08:13 +0900

    useradd 스크립트 수정

--- script/useradd_root.sh
+++ script/useradd_root.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-USERNAME=usertest
-PASSWORD="test1212!"
+USERNAME="test"
+PASSWORD="testtest"
 SHELL_PATH=/bin/bash
 
 
-is_exist=`cat /etc/passwd | grep "^$USERNAME:"`
+is_exist=$(cat /etc/passwd | grep "^$USERNAME:")
 if [[ $is_exist != "" ]]; then
 	echo "user name exist"
 	exit
 fi
 
-HOME_PATH=/home/$USERNAME
-if [ ! -d $HOME_PATH ]; then
-	mkdir $HOME_PATH
+HOME_PATH="/home/$USERNAME"
+if [ ! -d "$HOME_PATH" ]; then
+	mkdir -p "$HOME_PATH"
 else
 	echo "home directory exist"
 fi
 # useradd
-useradd $USERNAME -d $HOME_PATH
+useradd "$USERNAME" -d "$HOME_PATH"
 
 # /etc/passwd change
-sed -i "s#^$USERNAME:x:.*#$USERNAME:x:0:0::$HOME_PATH:$SHELL_PATH#" /etc/passwd
+sed -i "s#^$USERNAME:x:.*#$USERNAME:x:0:0:$USERNAME:$HOME_PATH:$SHELL_PATH#" /etc/passwd
 
 # /etc/group change
 sed -i "s#^$USERNAME:x:.*#$USERNAME:x:0:#" /etc/group
 
 # password setting
-yes $PASSWORD | passwd $USERNAME
+yes "$PASSWORD" | passwd "$USERNAME"
+
