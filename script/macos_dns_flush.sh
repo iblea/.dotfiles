b0f7fc255f5d06c2485b037563027fc10efa32bf
iblea <iblea@iasdf.com>
2025-07-08 08:55:01 +0900

    dns flush script add

--- script/macos_dns_flush.sh
+++ script/macos_dns_flush.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# <username> ALL = NOPASSWD: /usr/bin/pmset
+# <username> ALL=(root) NOPASSWD: /usr/bin/pmset
+# <username> ALL=(root) NOPASSWD: /usr/bin/dscacheutil -flushcache
+# <username> ALL=(root) NOPASSWD: /usr/bin/killall -HUP mDNSResponder
+
+sudo dscacheutil -flushcache
 sudo killall -HUP mDNSResponder
+
