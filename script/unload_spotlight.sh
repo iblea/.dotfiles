01ee1e2387406ea12a6830cd86ae0049f62bb412
iblea <iblea@iasdf.com>
2026-01-28 14:40:39 +0900

    명령어 업데이트

--- script/unload_spotlight.sh
+++ script/unload_spotlight.sh
@@ -3,7 +3,9 @@
 
 
 sudo mdutil -a -i off
-sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+# sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+sudo launchctl bootout system /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+sudo launchctl disable system/com.apple.metadata.mds
 
 # sudo mdutil -s /
 # sudo mdutil -a -s
