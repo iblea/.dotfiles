673caaee0f65684cfd7a5bf218ca2dfbafa46eaf
iblea <iblea@iasdf.com>
2026-03-11 07:18:30 +0000

    exit 0 제거

--- script/snapshot_qemuimg.sh
+++ script/snapshot_qemuimg.sh
@@ -3,8 +3,6 @@
 # 링킹 원본 파일 경로를 반환하지 않는 스크립트 현재 경로
 curpath=$(readlink -e $(dirname "$0"))
 cd "$curpath"
-echo "$curpath"
-exit 0
 
 SNAPSHOT_SAVE_FILE="$curpath/.name_of_snapshot_utm.txt"
 IMAGE_PATH=""
