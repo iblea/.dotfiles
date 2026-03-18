2f67ce66b4ad42b4becbcb943d6fa4354434e914
iblea <iblea@iasdf.com>
2026-03-18 10:17:56 +0900

    nvim: 플러그인에서 특정 디렉토리로 이동하는 현상에 대한 임시처리

--- vimrc/neovim/lua/plugins/leetcode.lua
+++ vimrc/neovim/lua/plugins/leetcode.lua
@@ -11,8 +11,22 @@ return {
     lang = "cpp",
     image_support = true,
   },
+  build = "sed -i'' -e 's/vim.api.nvim_set_current_dir(config.storage.home:absolute())/vim.api.nvim_set_current_dir(vim.fn.getcwd())/' lua/leetcode.lua",
   config = function(_, opts)
+    local lc_dir = vim.fn.stdpath("data") .. "/leetcode"
+    local _orig_cwd = vim.fn.getcwd()
+
     require("leetcode").setup(opts)
+
+    -- leetcode 파일 진입 시 cwd가 바뀌어 있으면 강제 복원
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        if vim.fn.getcwd():find(lc_dir, 1, true) then
+          pcall(vim.api.nvim_set_current_dir, _orig_cwd)
+        end
+      end,
+    })
+
     -- nui.nvim이 split 생성 시 winfixwidth/winfixheight를 켜서
     -- wincmd = 가 안 먹히는 문제 해결
     vim.api.nvim_create_autocmd("FileType", {
