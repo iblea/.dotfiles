9ef2253ba5c201e90f86525627d2ffa3c79c7ce6
iblea <iblea@iasdf.com>
2026-03-20 09:58:25 +0900

    nodejs 설치 스크립트 추가

--- script/install_neovim.sh
+++ script/install_neovim.sh
@@ -36,3 +36,18 @@ if [ -f "/opt/nvim/bin/nvim" ]; then
 fi
 
 
+if [ -z "$(command -v node)" ]; then
+    apt-get update
+    apt-get install -y ca-certificates curl gnupg
+    mkdir -p /etc/apt/keyrings
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+    NODE_MAJOR=22
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+    unset NODE_MAJOR
+
+    apt-get update
+    apt-get install -y nodejs
+fi
+
+
