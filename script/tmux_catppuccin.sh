870ec8679732e4cbb9ba66c1d1ebf2eb2582618b
iblea <iblea@iasdf.com>
2026-03-23 09:48:10 +0900

    catppuccin 추가

---
+++ script/tmux_catppuccin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ~/.config/tmux/catppuccin
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/catppuccin
+
--- tmux/is_vim.sh
+++ tmux/is_vim.sh
@@ -6,7 +6,11 @@
 tmux display-message -p '#{pane_title}' \
     | grep -iqE '(^|/)(view|l?n?vim?x?|fzf)(diff)?(\s|$)' && exit 0
 
-# 2. 로컬 프로세스 확인
+# 2. pane_current_command로 감지
+tmux display-message -p '#{pane_current_command}' \
+    | grep -iqE '^(view|l?n?vim?x?|fzf)(diff)?$' && exit 0
+
+# 3. 로컬 프로세스 확인
 ps -o state= -o comm= -t "$(tmux display-message -p '#{pane_tty}')" \
     | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$' && exit 0
 
