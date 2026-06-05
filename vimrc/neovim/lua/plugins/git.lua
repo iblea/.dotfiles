f536fe0f71e1d485fb79d5bb209ae37e863eaa3e
iblea <iblea@iasdf.com>
2026-06-05 17:43:05 +0900

    diffview.nvim 수정

--- vimrc/neovim/lua/plugins/git.lua
+++ vimrc/neovim/lua/plugins/git.lua
@@ -92,6 +92,131 @@ return {
     },
     opts = function()
       local actions = require("diffview.config").actions
+      local renderer = require("diffview.renderer")
+      local utils = require("diffview.utils")
+      local file_panel = require("diffview.scene.views.diff.file_panel")
+
+      local function setup_staged_first_file_panel()
+        local FilePanel = file_panel.FilePanel
+
+        if FilePanel._staged_first_patched then
+          return
+        end
+
+        local function file_components(files)
+          local components = { name = "files" }
+
+          for _, file in ipairs(files) do
+            table.insert(components, {
+              name = "file",
+              context = file,
+            })
+          end
+
+          return components
+        end
+
+        local function tree_components(tree, tree_options)
+          tree:update_statuses()
+
+          return utils.tbl_merge(
+            { name = "files" },
+            tree:create_comp_schema({
+              flatten_dirs = tree_options.flatten_dirs,
+            })
+          )
+        end
+
+        function FilePanel:update_components()
+          local conflicting_files
+          local staged_files
+          local working_files
+
+          self.files.sets = {
+            self.files.conflicting,
+            self.files.staged,
+            self.files.working,
+          }
+
+          if self.listing_style == "list" then
+            conflicting_files = file_components(self.files.conflicting)
+            staged_files = file_components(self.files.staged)
+            working_files = file_components(self.files.working)
+          elseif self.listing_style == "tree" then
+            conflicting_files = tree_components(
+              self.files.conflicting_tree,
+              self.tree_options
+            )
+            staged_files = tree_components(
+              self.files.staged_tree,
+              self.tree_options
+            )
+            working_files = tree_components(
+              self.files.working_tree,
+              self.tree_options
+            )
+          end
+
+          self.components = self.render_data:create_component({
+            { name = "path" },
+            {
+              name = "conflicting",
+              { name = "title" },
+              conflicting_files,
+              { name = "margin" },
+            },
+            {
+              name = "staged",
+              { name = "title" },
+              staged_files,
+              { name = "margin" },
+            },
+            {
+              name = "working",
+              { name = "title" },
+              working_files,
+              { name = "margin" },
+            },
+            {
+              name = "info",
+              { name = "title" },
+              { name = "entries" },
+            },
+          })
+
+          self.constrain_cursor = renderer.create_cursor_constraint({
+            self.components.conflicting.files.comp,
+            self.components.staged.files.comp,
+            self.components.working.files.comp,
+          })
+        end
+
+        function FilePanel:ordered_file_list()
+          if self.listing_style == "list" then
+            local list = {}
+
+            for _, file in self.files:iter() do
+              list[#list + 1] = file
+            end
+
+            return list
+          end
+
+          local nodes = utils.vec_join(
+            self.files.conflicting_tree.root:leaves(),
+            self.files.staged_tree.root:leaves(),
+            self.files.working_tree.root:leaves()
+          )
+
+          return vim.tbl_map(function(node)
+            return node.data
+          end, nodes)
+        end
+
+        FilePanel._staged_first_patched = true
+      end
+
+      setup_staged_first_file_panel()
 
       return {
         view = {
